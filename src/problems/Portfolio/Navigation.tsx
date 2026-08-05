fb6a79bb31686e8f8c923d1c85aa06595901459d
loafingbread <felixle96@gmail.com>
2026-08-04 23:54:37 -0700

    Layout navigation

--- src/problems/Portfolio/Navigation.tsx
+++ src/problems/Portfolio/Navigation.tsx
@@ -4,7 +4,24 @@ import styles from "./Navigation.module.css";
 function Navigation() {
   return (
     <div className="container">
-      <nav>Navigation</nav>
+      <nav className={styles.navigation} aria-label="Navigation Bar">
+        <div className={styles.left}>
+          <a className={styles.navHome} href="#">
+            Logo
+          </a>
+        </div>
+        <div className={styles.right}>
+          <a className={styles.navLink} href="#">
+            About
+          </a>
+          <a className={styles.navLink} href="#">
+            Work
+          </a>
+          <a className={styles.navLink} href="#">
+            Contact
+          </a>
+        </div>
+      </nav>
     </div>
   );
 }
