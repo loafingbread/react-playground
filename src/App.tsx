4cd1427846a877565f2b9b8793e9d1a0a81abb66
loafingbread <felixle96@gmail.com>
2026-08-05 13:19:09 -0700

    Add Security Cameras page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { NavLink, Route, Routes } from "react-router";
 import "./App.css";
 import { HomePage, PortfolioTemplate, ProblemsPage } from "./pages";
+import SecurityCameraPage from "./pages/SecurityCameraPage";
 
 function App() {
   return (
@@ -9,11 +10,13 @@ function App() {
         <NavLink to="/">Home</NavLink>
         <NavLink to="/problems">Problems</NavLink>
         <NavLink to="/portfolio-template">Portfolio Template</NavLink>
+        <NavLink to="/security-cameras">Security Cameras</NavLink>
       </nav>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/problems" element={<ProblemsPage />} />
         <Route path="/portfolio-template" element={<PortfolioTemplate />} />
+        <Route path="/security-cameras" element={<SecurityCameraPage />} />
       </Routes>
     </>
   );
---
+++ src/pages/SecurityCameraPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./SecurityCameraPage.module.css";
+
+function SecurityCameraPage() {
+  return <div className={styles.securityCameraPageWrapper}>Security</div>;
+}
+
+export default SecurityCameraPage;
